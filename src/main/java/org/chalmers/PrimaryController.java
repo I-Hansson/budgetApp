1119e425339e168e4a800e2fddc3b6618eb8ceea
Cronvall <47293083+Cronvall@users.noreply.github.com>
2022-09-01 10:54:17 +0200

    Update PrimaryController.java

--- src/main/java/org/chalmers/PrimaryController.java
+++ src/main/java/org/chalmers/PrimaryController.java
@@ -8,5 +8,6 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+        System.out.println("Rules?");
     }
 }
