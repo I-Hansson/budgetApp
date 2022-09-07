33a32d47728742f337fdb8c8e5c03cee352c5a9c
Cronvall <47293083+Cronvall@users.noreply.github.com>
2022-09-07 15:53:56 +0200

    Create Transaction.java
    Transaction boilerplate

---
+++ src/main/java/org/chalmers/model/Transaction.java
@@ -0,0 +1,33 @@
+package org.chalmers.model;
+
+import java.util.Date;
+
+public class Transaction {
+    private Date dateOfTransaction;
+    private double amount;
+    private String budgetPostName;
+    private String description;
+
+    public Transaction(double amount, String budgetPostName, String description){
+        this.dateOfTransaction = new Date(System.currentTimeMillis());
+        this.amount = amount;
+        this.budgetPostName = budgetPostName;
+        this.description = description;
+        //TODO if budgetPostName doesn't exist create it or throw error?
+        //TODO if amount surpasses budget-post's cap alert user and add it to post.
+    }
+
+    //Getters
+    public Date getDateOfTransaction() {
+        return new Date(dateOfTransaction.getTime());
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public String getBudgetPostName(){
+        return budgetPostName;
+    }
+    public String getDescription(){
+        return description;
+    }
+}
