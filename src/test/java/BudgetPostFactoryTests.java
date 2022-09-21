6eac862fd771da6916337b67d49dd6e141e03f4d
William <william.hp.frisk@gmail.com>
2022-09-21 15:10:46 +0200

    h

--- src/test/java/BudgetPostFactoryTests.java
+++ src/test/java/BudgetPostFactoryTests.java
@@ -1,27 +1,28 @@
 import org.chalmers.model.BudgetPost;
 import org.chalmers.model.BudgetPostFactory;
 import org.chalmers.model.Color;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class BudgetPostFactoryTests {
 
     @Test
     public void createBudgetPostNameReturnsCorrectInstance() {
         BudgetPost bp = BudgetPostFactory.createBudgetPost("test");
-        assertEquals(bp.getName() + bp.getBudgetCap(), "test0");
+        assertEquals("test0", bp.getName() + bp.getBudgetCap());
     }
 
     @Test
     public void createBudgetPostNameBudgetCapReturnsCorrectInstance(){
         BudgetPost bp = BudgetPostFactory.createBudgetPost("test", 100);
-        assertEquals(bp.getName() + bp.getBudgetCap(), "test100");
+        assertEquals( "test100", bp.getName() + bp.getBudgetCap());
     }
 
     @Test
     public void createBudgetPostNameBudgetCapColorReturnsCorrectInstance(){
         BudgetPost bp = BudgetPostFactory.createBudgetPost("test", 100, Color.BLACK);
-        assertEquals(bp.getName() + bp.getBudgetCap() + bp.getColorRGB()[0], "test1000");
+        assertEquals("test1000", bp.getName() + bp.getBudgetCap() + bp.getColorRGB()[0]);
     }
 }
