8e9e3389f98eb0ce860665c50cebe1886a07a584
William <william.hp.frisk@gmail.com>
2022-09-21 14:24:55 +0200

    Fixade något som jag sa till svante att fixa för 2 veckor sedan
    Tog mig 2 sekunder btw

--- src/main/java/org/chalmers/App.java
+++ src/main/java/org/chalmers/App.java
@@ -19,6 +19,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("LoginPage"), 1280, 860);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
