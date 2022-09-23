b6a52b063d68d6dfc95aa3846b3f86d8e0d30337
Isac Hansson <isachansson1218@gmail.com>
2022-09-23 12:01:39 +0200

    La till Budget klassen och tog data ifrån user klassen

--- src/main/java/org/chalmers/model/User.java
+++ src/main/java/org/chalmers/model/User.java
@@ -6,13 +6,12 @@ import java.util.Map;
 
 public class User {
 
-    private double startBalance;
-    private double currentBalance;
+
     private String name;
     private int id;
     private boolean instantiatd = false;
-    private Map<String, BudgetPost> budgetPosts = new HashMap<>();
-    private ArrayList<Transaction> recentTransactions = new ArrayList<>();
+    private ArrayList<Budget> budgets = new ArrayList<>();
+
     //List of investments
 
     public User(){
@@ -20,12 +19,8 @@ public class User {
     }
 
     //Getters
-    public double getCurrentBalance() {
-        return currentBalance;
-    }
-    public double getStartBalance(){
-        return currentBalance;
-    }
+
+
 
     public String getName(){
         return name;
@@ -34,9 +29,7 @@ public class User {
     public int getId(){
         return id;
     }
-    public ArrayList getRecentTransactions(){
-        return recentTransactions;
-    }
+
     //Methodology
 
 
@@ -44,40 +37,14 @@ public class User {
      * Gives us (the developers) a sample values to work with in development.
      */
     public void Instantiate(){
-        setStartBalance(10000);
-        currentBalance = 10000;
         name = "Oscar";
         id = 4;
         instantiatd = true;
     }
 
-    /**
-     * Sets the new start balance for each month.
-     * @param newStartBalance the new starting value for each month.
-     */
-    public void setStartBalance(double newStartBalance){
-        startBalance = newStartBalance;
-    }
 
-    /**
-     * Update the current balance.
-     * @param change the amount that changes, if expense use negative values.
-     */
-    public void updateBalance(double change){
-        currentBalance += change;
-    }
 
-    /**
-     * Add a NEW budget-post to the users budget planner.
-     * @param name the name of the new post.
-     * @param cap the maximum amount intended for this post.
-     */
-    public void addBudgetPost(String name, double cap){
-        if(!budgetPosts.containsKey(name)){
-            budgetPosts.put("test", new BudgetPost(name,cap));
-        } else{
-            //TODO Alert user that post already exists
-            System.out.println("Post " + name + " already exists");
-        }
-    }
+
+
+
 }
