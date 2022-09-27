a32b7ef6f8b26772d64f2fbc523329a518a87c2c
Isac Hansson <isachansson1218@gmail.com>
2022-09-27 22:12:35 +0200

    Fixat allt de svante ställde till med

--- src/main/java/org/chalmers/OverviewView.java
+++ src/main/java/org/chalmers/OverviewView.java
@@ -8,19 +8,17 @@ import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
-import org.chalmers.model.OverallSpentPieChart;
 
-import java.io.IOException;
+import javafx.scene.text.Text;
+
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.ResourceBundle;
 
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import org.chalmers.Overviewpiechart;
 
 
 public class OverviewView implements Initializable {
@@ -31,49 +29,25 @@ public class OverviewView implements Initializable {
     @FXML Text pastTransactionsTitelPanel;
 
 
-    @FXML Pane pieChartOverview;
-    @FXML PieChart overallSpentPieChart;
+    @FXML FlowPane PiechartFlowPane;
 
-    @FXML Pane pieChartConnectedInfo;
     @FXML TextArea overviewMessageTextArea;
     @FXML GridPane budgetPostsGridPane;
     @FXML Button newTransactionButton;
 
-
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        OverallSpentPieChart hej = new OverallSpentPieChart();
 
-        //this.overallSpentPieChart.setStyle("-fx-pie-label-visible:False;");
-        this.overallSpentPieChart.getData().addAll(hej.getData());
-
-
-        try {
-            updateBudgetPostPanel();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    public void updateBudgetPostPanel () throws IOException {
-
-        for (int i = 0; i < 4; i++) {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            fxmlLoader.setLocation(getClass().getResource("BudgetPostPanel.fxml"));
-            AnchorPane anchorPane = fxmlLoader.load();
-            BudgetPostCard budgetPostCard = fxmlLoader.getController();
-            budgetPostCard.setData();
-
-            budgetPostsGridPane.add(anchorPane, i, 0);
-
+       this.PiechartFlowPane.getChildren().add(new Overviewpiechart());
+       for(int i = 0;i<4; i++){
+            this.budgetPostsGridPane.add(new OverviewBudgetPost(),i,0);
         }
 
     }
 
 }
+
+
 
 
 
--- src/main/java/org/chalmers/PrimaryController.java
+++
@@ -1,7 +0,0 @@
-package org.chalmers;
-
-import java.io.IOException;
-import javafx.fxml.FXML;
-
-public class PrimaryController {
-}
