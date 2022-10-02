42f23dff0ef30a8437cb8dd1e82687f2b5774096
Isac Hansson <isachansson1218@gmail.com>
2022-10-02 17:27:48 +0200

    en animation

--- src/main/java/org/chalmers/OverviewBudgetPost.java
+++ src/main/java/org/chalmers/OverviewBudgetPost.java
@@ -1,5 +1,7 @@
 package org.chalmers;
 
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
@@ -7,6 +9,8 @@ import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.transform.Translate;
+import javafx.util.Duration;
 import org.chalmers.Controllers.BudgetPostPanelController;
 import org.chalmers.model.BudgetPost;
 
@@ -36,6 +40,17 @@ public class OverviewBudgetPost extends AnchorPane{
         budgetPostTotalAmount.setText( moneyLeft + " Kr");
         budgetPostCard.setStyle("-fx-background-color: rgba("+ color+" );");
         increasingPaneBudgetPost.setStyle("-fx-background-color: rgba("+ complementColor+" );");
+
+        increasingPaneBudgetPost.setPrefHeight(0);
+
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(increasingPaneBudgetPost);
+        translate.setDuration(Duration.millis(1000));
+        translate.setFromY(0 -250*procent);
+        translate.setToY(0);
+        translate.setCycleCount(1);
+        translate.play();
         increasingPaneBudgetPost.setPrefHeight(250*procent);
+
     }
 }
