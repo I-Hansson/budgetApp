fc9d3b8ccee5d610422aae83ff44ed889405a995
Cronvall <47293083+Cronvall@users.noreply.github.com>
2022-10-02 18:07:35 +0200

    setters to budgetPosts
    1. setName
    2. setCap
    3. addTransaction

--- src/main/java/org/chalmers/model/database/BudgetPostsDB.java
+++ src/main/java/org/chalmers/model/database/BudgetPostsDB.java
@@ -3,13 +3,16 @@ package org.chalmers.model.database;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class BudgetPostsDB {
 
     private DatabaseConnector connector;
+    private FileWriter file;
+    private JSONObject oldDB;
 
     public BudgetPostsDB(String postID){
         connector = new DatabaseConnector("src/main/database/budgetPosts/" + postID +".json");
@@ -29,7 +32,7 @@ public class BudgetPostsDB {
         return cap;
     }
 
-    public List getTransactions(){
+    public List<JSONObject> getTransactions(){
         ArrayList<JSONObject> result = new ArrayList<>();
         JSONArray transactions = (JSONArray) getBudgetPost().get("transactions");
         for(int i = 0; i < transactions.size(); i++){
@@ -38,7 +41,49 @@ public class BudgetPostsDB {
         }
         return result;
     }
-
-
     
+    public void setName(String newName){
+        oldDB.put("name", newName);
+    }
+
+    public void setCap(Double newCap){oldDB.put("cap", newCap);}
+
+    public void addTransaction(String description, Double total, String date){
+        JSONArray transactions = (JSONArray) oldDB.get("transactions");
+        JSONObject newTransactions = new JSONObject();
+        newTransactions.put("description", description);
+        newTransactions.put("total", total);
+        newTransactions.put("date", date);
+        transactions.add(newTransactions);
+        oldDB.put("transactions", transactions);
+    }
+
+
+    /**
+     * Call this method so that you can edit the database.
+     */
+    public void openSetters(){
+        try{
+            file = new FileWriter(connector.getDbPath());
+            oldDB = getBudgetPost();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Call this method to write and close new edits to the database.
+     * To edit again call on the openSetters() method
+     */
+    public void closeSetter(){
+        try{
+            file.write(oldDB.toJSONString());
+            file.flush();
+            file.close();
+            oldDB = null;
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
