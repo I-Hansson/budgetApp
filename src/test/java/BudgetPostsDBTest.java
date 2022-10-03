c3049193cfd04190a8c75220079eb8b9c17d9837
Cronvall <47293083+Cronvall@users.noreply.github.com>
2022-10-03 11:37:23 +0200

    Update BudgetPostsDBTest.java

--- src/test/java/BudgetPostsDBTest.java
+++ src/test/java/BudgetPostsDBTest.java
@@ -4,14 +4,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BudgetPostsDBTest {
 
+    BudgetPostsDB db = new BudgetPostsDB("test");
+
     @Test
     public void assertNewName(){
-        BudgetPostsDB db = new BudgetPostsDB("test");
-        String oldName = db.getName();
-        String newName = "Chin Yang";
+        String newName = "Kylskåp";
         db.openSetters();
-        db.setName(newName);
+            db.setName(newName);
         db.closeSetter();
-        assertEquals(newName, db.getName());
+
+        assertEquals(newName,db.getName());
+    }
+
+    @Test
+    public void assertNewCap(){
+        Double newCap = 4567.99;
+        db.openSetters();
+            db.setCap(newCap);
+        db.closeSetter();
+
+        assertEquals(newCap,db.getCap());
+    }
+
+    @Test
+    public void assertNewTransaction(){
+        Integer transactionsLength = db.getTransactions().size();
+        db.openSetters();
+            db.addTransaction("test transaction", 100.0, "202210032100");
+        db.closeSetter();
+
+        Integer newTransactionLength = db.getTransactions().size();
+        assertEquals(transactionsLength+1, newTransactionLength);
     }
 }
