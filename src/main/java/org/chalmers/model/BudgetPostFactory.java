cfa10227b3e394506516903d074b7dfdf0d343b9
Isac Hansson <isachansson1218@gmail.com>
2022-10-05 14:14:04 +0200

    Fixat Budgetpostfactory

--- src/main/java/org/chalmers/model/BudgetPostFactory.java
+++ src/main/java/org/chalmers/model/BudgetPostFactory.java
@@ -7,38 +7,19 @@ import javafx.scene.paint.Color;
  * a simpler interface than the BudgetPost constructor.
  */
 public class BudgetPostFactory {
-    //TODO Jonas sa något om statiska färger?
-    private static final Color[] colors = {
-            Color.RED,
-            Color.BLUE,
-            Color.GREEN,
-            Color.PURPLE,
-            Color.GRAY,
-            Color.PINK,
-            Color.BLACK,
-            Color.TURQUOISE,
-            Color.CHOCOLATE
-    };
 
-    private static int currentColorIndex = 0;
-
-    /*public static BudgetPost createBudgetPost(String name) {
-        BudgetPost bp = new BudgetPost(name, 0, Color.color(0.02, 0.20, 0.36));
-        bp.setColor(colors[currentColorIndex]);
-        currentColorIndex++;
+    public static BudgetPost createBudgetPost(String name) {
+        BudgetPost bp = new BudgetPost(name, 0, "5, 51, 92");
         return bp;
     }
 
     public static BudgetPost createBudgetPost(String name, double budgetCap) {
-        BudgetPost bp = new BudgetPost(name, budgetCap, Color.color(0.02, 0.20, 0.36));
-        bp.setColor(colors[currentColorIndex]);
-        currentColorIndex++;
+        BudgetPost bp = new BudgetPost(name, budgetCap, "5, 51, 92");
         return bp;
     }
 
-    public static BudgetPost createBudgetPost(String name, double budgetCap, Color color) {
-        BudgetPost bp = new BudgetPost(name, budgetCap, Color.color(0.02, 0.20, 0.36));
-        bp.setColor(color);
+    public static BudgetPost createBudgetPost(String name, double budgetCap, String color) {
+        BudgetPost bp = new BudgetPost(name, budgetCap, color);
         return bp;
-    }*/
+    }
 }
