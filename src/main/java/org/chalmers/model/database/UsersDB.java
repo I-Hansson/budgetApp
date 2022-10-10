a53f0c78e54810ef9dc821daa9eeffca7b9aa62c
Cronvall <47293083+Cronvall@users.noreply.github.com>
2022-10-10 20:34:48 +0200

    changes

--- src/main/java/org/chalmers/model/database/UsersDB.java
+++ src/main/java/org/chalmers/model/database/UsersDB.java
@@ -1,11 +1,9 @@
 package org.chalmers.model.database;
 
 import org.chalmers.model.Transaction;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.io.*;
-import java.nio.channels.FileChannel;
 import java.util.List;
 import java.util.Objects;
 
@@ -28,55 +26,6 @@ public class UsersDB {
         transactionsDB = new TransactionsDB(uid);
     }
 
-    public static void createUserDoc(String name, String pwd, Double balance, Double standardBalance){
-        try{
-            FileWriter incomingFile = new FileWriter("./src/main/database/users/incoming.json");
-            JSONObject jsonObject = new JSONObject();
-            try{
-                jsonObject.put("name", name);
-                jsonObject.put("password", pwd);
-                jsonObject.put("currentBalance", balance);
-                jsonObject.put("startBalance", standardBalance);
-                jsonObject.put("id", nextID);
-            } finally {
-                incomingFile.write(jsonObject.toJSONString());
-                incomingFile.flush();
-                incomingFile.close();
-            }
-            File srcFile = new File("./src/main/database/users/incoming.json");
-            File destFile = new File("./src/main/database/users/" + nextID + ".json");
-            FileChannel src = new FileInputStream(srcFile).getChannel();
-            FileChannel dest = new FileOutputStream(destFile).getChannel();
-            dest.transferFrom(src, 0, src.size());
-            createUserTransactionDoc();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        nextID++;
-    }
-
-
-    private static void createUserTransactionDoc(){
-        try{
-            File newFile = new File("./src/main/database/transactions/" + nextID + ".json");
-            if(newFile.createNewFile())
-                System.out.println("Transaction document created");
-            else
-                System.out.println("Document of that id already exists");
-
-            JSONObject jsonObject = new JSONObject();
-            JSONArray transactions = new JSONArray();
-            jsonObject.put("transactions", transactions);
-
-            FileWriter writer = new FileWriter("./src/main/database/transactions/" + nextID + ".json");
-            writer.write(jsonObject.toJSONString());
-            writer.flush();
-            writer.close();
-
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Only intended to be used in the UsersDB class, reads and returns the read document in full.
